d9cb46c8ebc43e91a5b7f287f3bf25e787be0867
git <git@hcgcloud.com>
2019-09-05 12:25:03 +0800

    Add files

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Update Server
+if [ "${SERVER_AUTOUPDATE}" == "1" ]; then
+    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 261140 +quit
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
